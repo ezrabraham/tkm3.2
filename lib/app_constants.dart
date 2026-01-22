c9c311a1d11494a3ab92656b94c22e42ba9d5ada
elianexchange <elianexchange@gmail.com>
2026-01-22 18:20:03 +0100

    Update event date from 2025 to 2026

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -7,12 +7,12 @@ abstract class FFAppConstants {
   static const int tick3 = 17;
   static const String eventName = 'Bruno Mars - The Romantic Tour';
   static const String eventVenue = 'Rogers Stadium';
-  static const String eventSchedule = 'Sun, May 24, 2025, 7:00pm';
+  static const String eventSchedule = 'Sun, May 24, 2026, 7:00pm';
   static const String eventVenue2 = 'Rogers Stadium, Toronto, ON';
   static const String eventPicture = 'assets/images/bruno.png';
   static const String topBar = 'Standard  Admission';
   static const String innerDetails =
-      'Sun, May 24, 2025, 7:00pm -  Rogers Stadium, Toronto, ON';
+      'Sun, May 24, 2026, 7:00pm -  Rogers Stadium, Toronto, ON';
   static const String seatTag = 'Zone: Lower Level Seating';
   static const String eventname2 = 'Bruno Mars - The Romantic Tour';
   static const String seat1 = '-';
@@ -21,6 +21,6 @@ abstract class FFAppConstants {
   ///
   ///
   static const String eventName3 = 'Bruno Mars - The Romantic Tour';
-  static const String locateDate = 'Toronto, ON\nSun, May 24, 2025, 7:00pm';
+  static const String locateDate = 'Toronto, ON\nSun, May 24, 2026, 7:00pm';
   static const String location = 'Toronto, ON';
 }
