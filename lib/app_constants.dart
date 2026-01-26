e2b991fd1a6eea31d9f8919e0c8390d4f883331b
elianexchange <elianexchange@gmail.com>
2026-01-26 16:31:00 +0100

    Add BTS WORLD TOUR 'ARIRANG' IN MUNICH event: Jul 11, 2026 at Allianz Arena

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,26 +1,26 @@
 
 abstract class FFAppConstants {
-  static const String sec = 'U9';
-  static const String row = '9';
-  static const int tick1 = 17;
-  static const int tick2 = 18;
-  static const int tick3 = 19;
-  static const String eventName = 'Bruno Mars - The Romantic Tour';
-  static const String eventVenue = 'Stade De France';
-  static const String eventSchedule = 'Sat, Jun 20, 2026, 7:00pm';
-  static const String eventVenue2 = 'Stade De France, Paris, France';
-  static const String eventPicture = 'assets/images/bruno.png';
+  static const String sec = '106';
+  static const String row = '4';
+  static const int tick1 = 7;
+  static const int tick2 = 8;
+  static const int tick3 = 9;
+  static const String eventName = 'BTS WORLD TOUR \'ARIRANG\' IN MUNICH';
+  static const String eventVenue = 'Allianz Arena';
+  static const String eventSchedule = 'Sat, Jul 11, 2026, 8:00pm';
+  static const String eventVenue2 = 'Allianz Arena, Munich, Germany';
+  static const String eventPicture = 'assets/images/BTS-01.png';
   static const String topBar = 'Standard  Admission';
   static const String innerDetails =
-      'Sat, Jun 20, 2026, 7:00pm -  Stade De France, Paris, France';
+      'Sat, Jul 11, 2026, 8:00pm -  Allianz Arena, Munich, Germany';
   static const String seatTag = 'Zone: Lower Level Seating';
-  static const String eventname2 = 'Bruno Mars - The Romantic Tour';
+  static const String eventname2 = 'BTS WORLD TOUR \'ARIRANG\' IN MUNICH';
   static const String seat1 = '-';
 
   ///
   ///
   ///
-  static const String eventName3 = 'Bruno Mars - The Romantic Tour';
-  static const String locateDate = 'Paris, France\nSat, Jun 20, 2026, 7:00pm';
-  static const String location = 'Paris, France';
+  static const String eventName3 = 'BTS WORLD TOUR \'ARIRANG\' IN MUNICH';
+  static const String locateDate = 'Munich, Germany\nSat, Jul 11, 2026, 8:00pm';
+  static const String location = 'Munich, Germany';
 }
