bc4a6c80d053740e26214346ef3b8e513123c605
elianexchange <elianexchange@gmail.com>
2026-01-26 10:51:59 +0100

    Update event details for Paris show: Jun 20, 2026 at Stade De France

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,18 +1,18 @@
 
 abstract class FFAppConstants {
-  static const String sec = 'A4';
-  static const String row = '8';
-  static const int tick1 = 15;
-  static const int tick2 = 16;
-  static const int tick3 = 17;
+  static const String sec = 'U9';
+  static const String row = '9';
+  static const int tick1 = 17;
+  static const int tick2 = 18;
+  static const int tick3 = 19;
   static const String eventName = 'Bruno Mars - The Romantic Tour';
-  static const String eventVenue = 'Rogers Stadium';
-  static const String eventSchedule = 'Sun, May 24, 2026, 7:00pm';
-  static const String eventVenue2 = 'Rogers Stadium, Toronto, ON';
+  static const String eventVenue = 'Stade De France';
+  static const String eventSchedule = 'Sat, Jun 20, 2026, 7:00pm';
+  static const String eventVenue2 = 'Stade De France, Paris, France';
   static const String eventPicture = 'assets/images/bruno.png';
   static const String topBar = 'Standard  Admission';
   static const String innerDetails =
-      'Sun, May 24, 2026, 7:00pm -  Rogers Stadium, Toronto, ON';
+      'Sat, Jun 20, 2026, 7:00pm -  Stade De France, Paris, France';
   static const String seatTag = 'Zone: Lower Level Seating';
   static const String eventname2 = 'Bruno Mars - The Romantic Tour';
   static const String seat1 = '-';
@@ -21,6 +21,6 @@ abstract class FFAppConstants {
   ///
   ///
   static const String eventName3 = 'Bruno Mars - The Romantic Tour';
-  static const String locateDate = 'Toronto, ON\nSun, May 24, 2026, 7:00pm';
-  static const String location = 'Toronto, ON';
+  static const String locateDate = 'Paris, France\nSat, Jun 20, 2026, 7:00pm';
+  static const String location = 'Paris, France';
 }
