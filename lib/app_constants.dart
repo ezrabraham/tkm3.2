2282131da86eb41ccbe06e31c08899733d9d17fb
elianexchange <elianexchange@gmail.com>
2026-01-27 12:23:23 +0100

    Add BTS WORLD TOUR 'ARIRANG' IN LOS ANGELES event: Sep 05, 2026 at SoFi Stadium

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,26 +1,26 @@
 
 abstract class FFAppConstants {
-  static const String sec = '126';
-  static const String row = '20';
+  static const String sec = '231';
+  static const String row = '21';
   static const int tick1 = 7;
   static const int tick2 = 8;
   static const int tick3 = 9;
-  static const String eventName = 'BTS WORLD TOUR \'ARIRANG\' IN ARLINGTON';
-  static const String eventVenue = 'AT&T Stadium';
-  static const String eventSchedule = 'Sun, Aug 16, 2026, 8:00pm';
-  static const String eventVenue2 = 'AT&T Stadium, Arlington, TX';
+  static const String eventName = 'BTS WORLD TOUR \'ARIRANG\' IN LOS ANGELES';
+  static const String eventVenue = 'SoFi Stadium';
+  static const String eventSchedule = 'Sat, Sep 05, 2026, 8:00pm';
+  static const String eventVenue2 = 'SoFi Stadium, Inglewood, CA';
   static const String eventPicture = 'assets/images/BTS-01.png';
   static const String topBar = 'Standard  Admission';
   static const String innerDetails =
-      'Sun, Aug 16, 2026, 8:00pm -  AT&T Stadium, Arlington, TX';
+      'Sat, Sep 05, 2026, 8:00pm -  SoFi Stadium, Inglewood, CA';
   static const String seatTag = 'Zone: Lower Level Seating';
-  static const String eventname2 = 'BTS WORLD TOUR \'ARIRANG\' IN ARLINGTON';
+  static const String eventname2 = 'BTS WORLD TOUR \'ARIRANG\' IN LOS ANGELES';
   static const String seat1 = '-';
 
   ///
   ///
   ///
-  static const String eventName3 = 'BTS WORLD TOUR \'ARIRANG\' IN ARLINGTON';
-  static const String locateDate = 'Arlington, TX\nSun, Aug 16, 2026, 8:00pm';
-  static const String location = 'Arlington, TX';
+  static const String eventName3 = 'BTS WORLD TOUR \'ARIRANG\' IN LOS ANGELES';
+  static const String locateDate = 'Inglewood, CA\nSat, Sep 05, 2026, 8:00pm';
+  static const String location = 'Inglewood, CA';
 }
