a8200d1deb1350ff6abf833594c8b5d199f09962
elianexchange <elianexchange@gmail.com>
2026-02-05 18:24:10 +0100

    Show '-' instead of seat numbers for General Admission tickets in transfer modal

--- lib/components/transfer_page_widget.dart
+++ lib/components/transfer_page_widget.dart
@@ -376,7 +376,7 @@ class _TransferPageWidgetState extends State<TransferPageWidget> {
                                       ),
                                 ),
                                 Text(
-                                  FFAppConstants.tick1.toString(),
+                                  FFAppConstants.isGeneralAdmission ? '-' : FFAppConstants.tick1.toString(),
                                   textAlign: TextAlign.center,
                                   style: FlutterFlowTheme.of(context)
                                       .bodyLarge
@@ -529,7 +529,7 @@ class _TransferPageWidgetState extends State<TransferPageWidget> {
                                       ),
                                 ),
                                 Text(
-                                  FFAppConstants.tick2.toString(),
+                                  FFAppConstants.isGeneralAdmission ? '-' : FFAppConstants.tick2.toString(),
                                   textAlign: TextAlign.center,
                                   style: FlutterFlowTheme.of(context)
                                       .bodyLarge
@@ -682,7 +682,7 @@ class _TransferPageWidgetState extends State<TransferPageWidget> {
                                       ),
                                 ),
                                 Text(
-                                  FFAppConstants.tick3.toString(),
+                                  FFAppConstants.isGeneralAdmission ? '-' : FFAppConstants.tick3.toString(),
                                   textAlign: TextAlign.center,
                                   style: FlutterFlowTheme.of(context)
                                       .bodyLarge
